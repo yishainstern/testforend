05d1d0d0b5509765f2d86d61dbeb936974df4a82
yishainstern <syishai@cambium.oc.il>
2017-07-19 22:55:13 +0300

    e

--- app/scripts/controllers/enterController.js
+++ app/scripts/controllers/enterController.js
@@ -19,18 +19,21 @@ angular.module('sbAdminApp').controller('enterController', ['$scope', '$timeout'
     $scope.display_login_text = 'משתמש כבר קיים';
     $scope.show_loader = false;
     $scope.log_show = true;
+    
+    //Use regular expression to check if the email is a valid email address 
     $scope.valid_email = function(email){
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
     }
     
+    //Show test of error or success.
     $scope.show_error = function(str,flag){
         $scope.display_sgin_text = str;
         $scope.display_login_text = str; 
         $scope[flag] = true;
-         $scope.show_loader = false;
+        $scope.show_loader = false;
     }
-
+    //If the sign-in process was finished properly, sign-in and move to the next page.
     $scope.success_sgin = function(data){
         data = JSON.parse(data);
         console.log(data);
@@ -43,12 +46,12 @@ angular.module('sbAdminApp').controller('enterController', ['$scope', '$timeout'
             $scope.show_error('user alredy exsists','error_sign');
         }
     }
-
+    //When the sign-in process fails. 
     $scope.fail_sgin = function(data){
         $scope.show_error('some failer..try agin','error_sign');        
         console.log(data);
     }
-
+    //When the log-in process returns from the server with a success message. 
     $scope.success_log_in = function(data){
         data = JSON.parse(data);
         console.log(data);
@@ -63,11 +66,11 @@ angular.module('sbAdminApp').controller('enterController', ['$scope', '$timeout'
             $scope.show_error('rong user name...user name does not exsit','error_login');
         }        
     }
-
+    //When the log-in process fails. 
     $scope.fail_log_in = function(data){
         $scope.show_error('some failer..try agin','error_login'); 
     }
-
+    //Run the "sign-in" process. check if details are valid. If  they valid send them to the server, else notice the user of the problem
     $scope.sgin_task = function(form_naame){
         $scope.show_loader = true;
         if (!$scope.sginup_details.userName || !$scope.sginup_details.password ||!$scope.sginup_details.user_email ){
@@ -77,8 +80,7 @@ angular.module('sbAdminApp').controller('enterController', ['$scope', '$timeout'
         if (!$scope.valid_email($scope.sginup_details.user_email)){
             $scope.show_error('email not valid','error_sign');        
             return;
-        }
-       
+        }  
         $scope.display_sgin_text = '';        
         $scope.error_sign = false;
         service.ajaxfunc('sgin_up',form_naame,false)
