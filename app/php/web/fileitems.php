1afe92087de61c5393540c38542db7e6bf8667db
yishainstern <syishai@cambium.oc.il>
2017-10-31 10:45:08 +0200

    ere

---
+++ app/php/web/fileitems.php
@@ -0,0 +1,42 @@
+<?php
+	namespace Debugger;
+	/**
+	* 
+	*/
+	class mainitem{
+		private $name;
+		private $values;
+		
+	}
+	/**
+	* 
+	*/
+	class subitem{
+		private $values;
+		function __construct(){
+			
+		}
+	}
+	/**
+	* 
+	*/
+	class rawitem
+	{
+		
+		function __construct()
+		{
+			# code...
+		}
+	}
+	/**
+	* 
+	*/
+	class allitems
+	{
+		
+		function __construct()
+		{
+			# code...
+		}
+	}
+?>
