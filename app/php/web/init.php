ede43e2dbb7c82f53ffd25f0fe737c5d2f28fc9d
rotba <rotba@post.bgu.ac.il>
2018-08-12 16:36:57 +0300

    nop

--- app/php/web/init.php
+++ app/php/web/init.php
@@ -124,7 +124,6 @@
 		// use
 		// $mail->Host = gethostbyname('smtp.gmail.com');
 		// if your network does not support SMTP over IPv6
-
 		//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
 		$mail->Port = 587;
 		//Set the encryption system to use - ssl (deprecated) or tls
